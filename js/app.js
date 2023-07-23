580e73284e16b28fda83648c14f076e70c94883f
dennis benavides <denizponce16@gmail.com>
2023-07-22 22:00:46 -0500

    seccion header->contenedor incluye menu finalizada

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+
+document.addEventListener('DOMContentLoaded', () => {
+    eventos();
+})
+
+const eventos = () => {
+    menu.addEventListener('click', abrirMenu);
+}
+
+const abrirMenu = () => {
+    navegacion.classList.remove('ocultar')
+    botonCerrar();
+};
+
+const botonCerrar = () => {
+    const btnCerrar = document.createElement('p');
+    const overlay = document.createElement('div');
+    overlay.classList.add('pantalla-completa');
+    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    btnCerrar.textContent = 'x';
+    btnCerrar.classList.add('btn-cerrar');
+    navegacion.appendChild(btnCerrar);
+    cerrarMenu(btnCerrar, overlay);
+}
+
+const cerrarMenu = (boton, overlay) => {
+    boton.addEventListener('click', () => {
+        navegacion.classList.add('ocultar');
+        overlay.remove();
+    })
+
+    overlay.onclick = function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar');
+        boton.remove();
+    }
+}
