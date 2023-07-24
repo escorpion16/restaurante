7ae07b263c24c78518600186643ca3d990750071
dennis benavides <denizponce16@gmail.com>
2023-07-24 12:31:15 -0500

    seccion separador imagen

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const menu = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
 const imagenes =  document.querySelectorAll('img');
-
+const btnTodos = document.querySelector('.todos');
+const btnEnsaladas = document.querySelector('.ensaladas');
+const btnPasta = document.querySelector('.pasta');
+const btnPostres = document.querySelector('.postres');
+const btnPizza = document.querySelector('pizza');
+const contenedorPlatillos = document.querySelector('.platillos');
 document.addEventListener('DOMContentLoaded', () => {
     eventos();
+    platillos();
 })
 
 const eventos = () => {
@@ -19,11 +25,12 @@ const botonCerrar = () => {
     const btnCerrar = document.createElement('p');
     const overlay = document.createElement('div');
     overlay.classList.add('pantalla-completa');
-    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
     const body = document.querySelector('body');
+    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
     body.appendChild(overlay);
     btnCerrar.textContent = 'x';
     btnCerrar.classList.add('btn-cerrar');
+    
     navegacion.appendChild(btnCerrar);
     cerrarMenu(btnCerrar, overlay);
 }
@@ -32,14 +39,15 @@ const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
             const imagen = entry.target;
-            observer.unobserver(imagen);
+            imagen.src = imagen.dataset.src;
+            observer.unobserve(imagen);
         }
     })
 })
 
 
 imagenes.forEach( imagen => {
-    imagen.src = imagen.dataset.src;
+    // imagen.src = imagen.dataset.src;
     observer.observe(imagen);
 })
 
@@ -47,6 +55,7 @@ const cerrarMenu = (boton, overlay) => {
     boton.addEventListener('click', () => {
         navegacion.classList.add('ocultar');
         overlay.remove();
+        boton.remove();
     })
 
     overlay.onclick = function(){
@@ -56,4 +65,46 @@ const cerrarMenu = (boton, overlay) => {
     }
 }
 
+const platillos = () => {
+    let platillosArreglo = [];
+    const platillos = document.querySelectorAll('.platillo');
+    platillos.forEach(platillo => platillosArreglo = [...platillosArreglo, platillo])
 
+    const ensaladas = platillosArreglo.filter(ensalada => ensalada.getAttribute('data-platillo') === 'ensalada');
+    const pastas = platillosArreglo.filter(pasta => pasta.getAttribute('data-platillo') === 'pasta');
+    const pizzas = platillosArreglo.filter(pizza => pizza.getAttribute('data-platillo') === 'pizza');
+    const postres = platillosArreglo.filter(postre => postre.getAttribute('data-platillo') === 'postre');
+
+    mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
+}
+
+const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) => {
+    btnEnsaladas.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        ensaladas.forEach(ensalada=> contenedorPlatillos.appendChild(ensalada));
+    });
+
+    btnPasta.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+         pastas.forEach(pasta=> contenedorPlatillos.appendChild(pasta));
+    });
+
+    btnPizza.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        pizzas.forEach(pizza=> contenedorPlatillos.appendChild(pizza));
+    });
+    btnPostres.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        postres.forEach(postre=> contenedorPlatillos.appendChild(postre));
+    });
+    btnTodos.addEventListener('click',()=>{
+        limpiarHtml(contenedorPlatillos);
+        todos.forEach(todo=> contenedorPlatillos.appendChild(todo));
+    });
+}
+
+const limpiarHtml = (contenedor) => {
+    while(contenedor.firstChild){
+        contenedor.removeChild(contenedor.firstChild);
+    }
+}
